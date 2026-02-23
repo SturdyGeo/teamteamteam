fc285095e000bf5cc41863c4e41c15da3d93db49
James <129238040+jamesk411@users.noreply.github.com>
2026-02-22 22:45:12 -0700

    style(web): enlarge unauth landing boss logo

--- apps/web/src/routes/home-page.tsx
+++ apps/web/src/routes/home-page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export function HomePage(): React.JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center gap-6">
-      <img src={bossLogo} alt="Boss logo" className="h-28 w-28 object-contain" />
+      <img src={bossLogo} alt="Boss logo" className="h-44 w-44 object-contain md:h-52 md:w-52" />
       <Button asChild className="rounded-full px-8">
         <Link to="/login">Sign in</Link>
       </Button>
